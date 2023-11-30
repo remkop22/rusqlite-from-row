0d8251b28e523ca9fc4e111baa41eb08cde56cbd
Remo Pas <remo.pas22@proton.me>
2023-11-30 11:07:35 +0100

    remove darling

--- rusqlite-from-row-derive/src/lib.rs
+++ rusqlite-from-row-derive/src/lib.rs
@@ -1,93 +1,96 @@
-use darling::{ast::Data, Error, FromDeriveInput, FromField, ToTokens};
+use std::borrow::Cow;
+
+// use darling::{ast::Data, Error, FromDeriveInput, FromField, ToTokens};
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Result};
+use syn::{
+    meta::ParseNestedMeta, parse_macro_input, parse_str, spanned::Spanned, Attribute, Data,
+    DataStruct, DeriveInput, Error, Field, Fields, LitStr, Result, Type,
+};
 
 /// Calls the fallible entry point and writes any errors to the tokenstream.
 #[proc_macro_derive(FromRow, attributes(from_row))]
 pub fn derive_from_row(input: TokenStream) -> TokenStream {
     let derive_input = parse_macro_input!(input as DeriveInput);
-    match try_derive_from_row(&derive_input) {
-        Ok(result) => result,
-        Err(err) => err.write_errors().into(),
-    }
+
+    try_derive_from_row(derive_input)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
 }
 
 /// Fallible entry point for generating a `FromRow` implementation
-fn try_derive_from_row(input: &DeriveInput) -> std::result::Result<TokenStream, Error> {
-    let from_row_derive = DeriveFromRow::from_derive_input(input)?;
-    Ok(from_row_derive.generate()?)
+fn try_derive_from_row(input: DeriveInput) -> Result<TokenStream2> {
+    let from_row_derive = DeriveFromRow::parse(input)?;
+
+    Ok(from_row_derive.generate())
 }
 
 /// Main struct for deriving `FromRow` for a struct.
-#[derive(Debug, FromDeriveInput)]
-#[darling(
-    attributes(from_row),
-    forward_attrs(allow, doc, cfg),
-    supports(struct_named)
-)]
 struct DeriveFromRow {
     ident: syn::Ident,
     generics: syn::Generics,
-    data: Data<(), FromRowField>,
+    data: Vec<FromRowField>,
 }
 
 impl DeriveFromRow {
-    /// Validates all fields
-    fn validate(&self) -> Result<()> {
-        for field in self.fields() {
-            field.validate()?;
+    fn parse(input: DeriveInput) -> Result<Self> {
+        let DeriveInput {
+            ident,
+            generics,
+            data:
+                Data::Struct(DataStruct {
+                    fields: Fields::Named(fields),
+                    ..
+                }),
+            ..
+        } = input
+        else {
+            return Err(Error::new(
+                input.span(),
+                "expected struct with named fields",
+            ));
+        };
+
+        let mut data = Vec::new();
+
+        for field in fields.named {
+            data.push(FromRowField::parse(field)?);
         }
 
-        Ok(())
+        Ok(Self {
+            ident,
+            generics,
+            data,
+        })
     }
 
-    /// Generates any additional where clause predicates needed for the fields in this struct.
-    fn predicates(&self) -> Result<Vec<TokenStream2>> {
+    fn predicates(&self) -> Vec<TokenStream2> {
         let mut predicates = Vec::new();
 
-        for field in self.fields() {
-            field.add_predicates(&mut predicates)?;
+        for field in &self.data {
+            field.add_predicates(&mut predicates);
         }
 
-        Ok(predicates)
-    }
-
-    /// Provides a slice of this struct's fields.
-    fn fields(&self) -> &[FromRowField] {
-        match &self.data {
-            Data::Struct(fields) => &fields.fields,
-            _ => panic!("invalid shape"),
-        }
+        predicates
     }
 
     /// Generate the `FromRow` implementation.
-    fn generate(self) -> Result<TokenStream> {
-        self.validate()?;
-
+    fn generate(self) -> TokenStream2 {
         let ident = &self.ident;
 
         let (impl_generics, ty_generics, where_clause) = self.generics.split_for_impl();
-        let original_predicates = where_clause.clone().map(|w| &w.predicates).into_iter();
-        let predicates = self.predicates()?;
+        let original_predicates = where_clause.map(|w| &w.predicates).into_iter();
+        let predicates = self.predicates();
 
-        let is_all_null_fields = self
-            .fields()
-            .iter()
-            .map(|f| f.generate_is_all_null())
-            .collect::<syn::Result<Vec<_>>>()?;
+        let is_all_null_fields = self.data.iter().map(|f| f.generate_is_all_null());
 
-        let try_from_row_fields = self
-            .fields()
-            .iter()
-            .map(|f| f.generate_try_from_row())
-            .collect::<syn::Result<Vec<_>>>()?;
+        let try_from_row_fields = self.data.iter().map(|f| f.generate_try_from_row());
 
-        Ok(quote! {
+        quote! {
             impl #impl_generics rusqlite_from_row::FromRow for #ident #ty_generics where #(#original_predicates),* #(#predicates),* {
                 fn try_from_row_prefixed(
-                    row: &rusqlite_from_row::rusqlite::Row, 
+                    row: &rusqlite_from_row::rusqlite::Row,
                     prefix: Option<&str>
                 ) -> std::result::Result<Self, rusqlite_from_row::rusqlite::Error> {
                     Ok(Self {
@@ -96,82 +99,58 @@ impl DeriveFromRow {
                 }
 
                 fn is_all_null(
-                    row: &rusqlite_from_row::rusqlite::Row, 
+                    row: &rusqlite_from_row::rusqlite::Row,
                     prefix: Option<&str>
                 ) -> std::result::Result<bool, rusqlite_from_row::rusqlite::Error> {
                     Ok(#(#is_all_null_fields)&&*)
                 }
             }
         }
-        .into())
     }
 }
 
 /// A single field inside of a struct that derives `FromRow`
-#[derive(Debug, FromField)]
-#[darling(attributes(from_row), forward_attrs(allow, doc, cfg))]
 struct FromRowField {
     /// The identifier of this field.
-    ident: Option<syn::Ident>,
+    ident: syn::Ident,
     /// The type specified in this field.
     ty: syn::Type,
-    /// Wether to flatten this field. Flattening means calling the `FromRow` implementation
-    /// of `self.ty` instead of extracting it directly from the row.
-    #[darling(default)]
-    flatten: bool,
-    /// Can only be used in combination with flatten. Will prefix all fields of the nested struct
-    /// with this string. Can be useful for joins with overlapping names.
-    prefix: Option<String>,
-    /// Optionaly use this type as the target for `FromRow` or `FromSql`, and then
-    /// call `TryFrom::try_from` to convert it the `self.ty`.
-    try_from: Option<String>,
-    /// Optionaly use this type as the target for `FromRow` or `FromSql`, and then
-    /// call `From::from` to convert it the `self.ty`.
-    from: Option<String>,
-    /// Override the name of the actual sql column instead of using `self.ident`.
-    /// Is not compatible with `flatten` since no column is needed there.
-    rename: Option<String>,
+    attrs: FromRowAttrs,
 }
 
 impl FromRowField {
-    /// Checks wether this field has a valid combination of attributes
-    fn validate(&self) -> Result<()> {
-        if self.from.is_some() && self.try_from.is_some() {
-            return Err(Error::custom(
-                r#"can't combine `#[from_row(from = "..")]` with `#[from_row(try_from = "..")]`"#,
-            )
-            .into());
-        }
+    pub fn parse(field: Field) -> Result<Self> {
+        let mut attrs = FromRowAttrs::default();
 
-        if self.rename.is_some() && self.flatten {
-            return Err(Error::custom(
-                r#"can't combine `#[from_row(flatten)]` with `#[from_row(rename = "..")]`"#,
-            )
-            .into());
-        }
+        attrs.parse(field.attrs)?;
 
-        Ok(())
+        Ok(Self {
+            ident: field.ident.expect("should be named"),
+            ty: field.ty,
+            attrs,
+        })
     }
 
     /// Returns a tokenstream of the type that should be returned from either
     /// `FromRow` (when using `flatten`) or `FromSql`.
-    fn target_ty(&self) -> Result<TokenStream2> {
-        if let Some(from) = &self.from {
-            Ok(from.parse()?)
-        } else if let Some(try_from) = &self.try_from {
-            Ok(try_from.parse()?)
+    fn target_ty(&self) -> &Type {
+        if let Some(from) = &self.attrs.from {
+            from
+        } else if let Some(try_from) = &self.attrs.try_from {
+            try_from
         } else {
-            Ok(self.ty.to_token_stream())
+            &self.ty
         }
     }
 
     /// Returns the name that maps to the actuall sql column
     /// By default this is the same as the rust field name but can be overwritten by `#[from_row(rename = "..")]`.
-    fn column_name(&self) -> String {
-        self.rename
+    fn column_name(&self) -> Cow<str> {
+        self.attrs
+            .rename
             .as_ref()
-            .map(Clone::clone)
-            .unwrap_or_else(|| self.ident.as_ref().unwrap().to_string())
+            .map(Cow::from)
+            .unwrap_or_else(|| self.ident.to_string().into())
     }
 
     /// Pushes the needed where clause predicates for this field.
@@ -181,64 +160,60 @@ impl FromRowField {
     /// and when using either `from` or `try_from` attributes it additionally pushes this bound:
     /// `T: std::convert::From<R>`, where `T` is the type specified in the struct and `R` is the
     /// type specified in the `[try]_from` attribute.
-    fn add_predicates(&self, predicates: &mut Vec<TokenStream2>) -> Result<()> {
-        let target_ty = &self.target_ty()?;
+    fn add_predicates(&self, predicates: &mut Vec<TokenStream2>) {
+        let target_ty = self.target_ty();
         let ty = &self.ty;
 
-        predicates.push(if self.flatten {
+        predicates.push(if self.attrs.flatten {
             quote! (#target_ty: rusqlite_from_row::FromRow)
         } else {
             quote! (#target_ty: rusqlite_from_row::rusqlite::types::FromSql)
         });
 
-        if self.from.is_some() {
+        if self.attrs.from.is_some() {
             predicates.push(quote!(#ty: std::convert::From<#target_ty>))
-        } else if self.try_from.is_some() {
+        } else if self.attrs.try_from.is_some() {
             let try_from = quote!(std::convert::TryFrom<#target_ty>);
 
             predicates.push(quote!(#ty: #try_from));
             predicates.push(quote!(rusqlite_from_row::rusqlite::Error: std::convert::From<<#ty as #try_from>::Error>));
             predicates.push(quote!(<#ty as #try_from>::Error: std::fmt::Debug));
         }
-
-        Ok(())
     }
 
-    fn generate_is_all_null(&self) -> Result<TokenStream2> {
+    fn generate_is_all_null(&self) -> TokenStream2 {
         let column_name = self.column_name();
-        let target_ty = self.target_ty()?;
+        let target_ty = self.target_ty();
 
-        let line = if self.flatten {
-            let prefix = if let Some(prefix) = &self.prefix {
+        if self.attrs.flatten {
+            let prefix = if let Some(prefix) = &self.attrs.prefix {
                 quote!(Some(&(prefix.unwrap_or("").to_string() + #prefix)))
-            }else {
+            } else {
                 quote!(prefix)
             };
 
             quote!(<#target_ty as rusqlite_from_row::FromRow>::is_all_null(row, #prefix)?)
-        }else {
-            quote!{
+        } else {
+            quote! {
                 rusqlite_from_row::rusqlite::Row::get_ref::<&str>(
-                    row, 
+                    row,
                     &(prefix.unwrap_or("").to_string() + #column_name)
                 )? == rusqlite_from_row::rusqlite::types::ValueRef::Null
             }
-        };
-
-        Ok(line)
+        }
     }
 
     /// Generate the line needed to retrieve this field from a row when calling `try_from_row`.
-    fn generate_try_from_row(&self) -> Result<TokenStream2> {
-        let ident = self.ident.as_ref().unwrap();
+    fn generate_try_from_row(&self) -> TokenStream2 {
+        let ident = &self.ident;
         let column_name = self.column_name();
         let field_ty = &self.ty;
-        let target_ty = self.target_ty()?;
+        let target_ty = self.target_ty();
 
-        let mut base = if self.flatten {
-            let prefix = if let Some(prefix) = &self.prefix {
+        let mut base = if self.attrs.flatten {
+            let prefix = if let Some(prefix) = &self.attrs.prefix {
                 quote!(Some(&(prefix.unwrap_or("").to_string() + #prefix)))
-            }else {
+            } else {
                 quote!(prefix)
             };
 
@@ -247,12 +222,65 @@ impl FromRowField {
             quote!(rusqlite_from_row::rusqlite::Row::get::<&str, #target_ty>(row, &(prefix.unwrap_or("").to_string() + #column_name))?)
         };
 
-        if self.from.is_some() {
+        if self.attrs.from.is_some() {
             base = quote!(<#field_ty as std::convert::From<#target_ty>>::from(#base));
-        } else if self.try_from.is_some() {
+        } else if self.attrs.try_from.is_some() {
             base = quote!(<#field_ty as std::convert::TryFrom<#target_ty>>::try_from(#base)?);
         };
 
-        Ok(quote!(#ident: #base))
+        quote!(#ident: #base)
+    }
+}
+
+#[derive(Default)]
+struct FromRowAttrs {
+    /// Wether to flatten this field. Flattening means calling the `FromRow` implementation
+    /// of `self.ty` instead of extracting it directly from the row.
+    flatten: bool,
+    /// Can only be used in combination with flatten. Will prefix all fields of the nested struct
+    /// with this string. Can be useful for joins with overlapping names.
+    prefix: Option<String>,
+    /// Optionaly use this type as the target for `FromRow` or `FromSql`, and then
+    /// call `TryFrom::try_from` to convert it the `self.ty`.
+    try_from: Option<Type>,
+    /// Optionaly use this type as the target for `FromRow` or `FromSql`, and then
+    /// call `From::from` to convert it the `self.ty`.
+    from: Option<Type>,
+    /// Override the name of the actual sql column instead of using `self.ident`.
+    /// Is not compatible with `flatten` since no column is needed there.
+    rename: Option<String>,
+}
+
+impl FromRowAttrs {
+    fn parse(&mut self, attrs: Vec<Attribute>) -> Result<()> {
+        for attr in attrs {
+            if !attr.meta.path().is_ident("from_row") {
+                continue;
+            }
+
+            attr.parse_nested_meta(|meta| self.parse_one(meta))?;
+        }
+
+        Ok(())
+    }
+
+    fn parse_one(&mut self, meta: ParseNestedMeta) -> Result<()> {
+        if meta.path.is_ident("flatten") {
+            self.flatten = true;
+        } else if meta.path.is_ident("prefix") {
+            let prefix: LitStr = meta.value()?.parse()?;
+            self.prefix = Some(prefix.value());
+        } else if meta.path.is_ident("try_from") {
+            let try_from: LitStr = meta.value()?.parse()?;
+            self.try_from = Some(parse_str(&try_from.value())?);
+        } else if meta.path.is_ident("from") {
+            let from: LitStr = meta.value()?.parse()?;
+            self.from = Some(parse_str(&from.value())?);
+        } else if meta.path.is_ident("rename") {
+            let rename: LitStr = meta.value()?.parse()?;
+            self.rename = Some(rename.value());
+        }
+
+        Ok(())
     }
 }
